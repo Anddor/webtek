2cfe39373954b4215fe5afb8b26944126559e76d
Andreas Dørum <andreas.doerum@gmail.com>
2015-11-03 22:59:28 +0100

    add exercise2

--- andreahd/assignment7/todo-list.js
+++ andreahd/assignment7/todo-list.js
@@ -1,32 +1,54 @@
 
 function init() {
     "use strict";
-    var clearBtn, addBtn, taskInput, taskArea, done;
+    var clearBtn, addBtn, taskInput, taskArea, tasks;
     clearBtn = document.getElementById("clear");
     addBtn = document.getElementById("add");
     taskInput = document.getElementById("input");
     taskArea = document.getElementById("task-area");
-    done = [];
     
     function handleAddTask() {
-        var text, task, btn;
+        if (taskInput.value === "") {
+            // No empty tasks
+            return;
+        }
+        var text, task, check, btn;
         task = document.createElement('div');
+        task.setAttribute("class", "task");
         
+        text = document.createTextNode(taskInput.value);
+        check = document.createElement("INPUT");
+        check.setAttribute("type", "checkbox");
+        check.setAttribute("class", "checkbox");
         
-        text = document.createElement('p');
-        btn = document.createElement('button');
+        task.appendChild(check);
+        task.appendChild(text);
         
-        task.appendChild(btn);
-        
-        
-        btn.addEventListener('click', function (event) {
-            console.log("done!");
-        });
+        taskArea.appendChild(task);
+        taskInput.value = "";
+        taskInput.focus();
     }
     
+    function handleClear() {
+        
+        var i, task, check, tasks;
+        tasks = taskArea.getElementsByClassName("task");
+        console.log(tasks);
+        
+        for (i = 0; i < tasks.length; i += 1) {
+            task = tasks[i];
+            check = task.getElementsByTagName("input")[0];
+            console.log(task);
+            
+            if (check.checked) {
+                console.log("checked!");
+                taskArea.removeChild(task);
+                i -= 1; //There is now one less item in the list, so we need to take one step backwards.
+            }
+        }
+    }
+    
+    clearBtn.addEventListener('click', handleClear);
     addBtn.addEventListener('click', handleAddTask);
-    
-    
-    
 }
 window.addEventListener("load", init);
